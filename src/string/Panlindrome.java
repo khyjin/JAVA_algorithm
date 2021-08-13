0b4490e50d08658fd70eaa9eef30d92263629f78
KIMHYEJIN <khyjin00@gmail.com>
2021-08-13 23:45:41 +0900

    upload 08.13

--- src/string/Panlindrome.java
+++ src/string/Panlindrome.java
@@ -1,30 +1,34 @@
 package string;
 
+import java.util.Scanner;
+
 public class Panlindrome {
 
 	public String solution(String str) {
 		String answer = "";
+		String[] word = str.split("");
+		int len = str.length()-1;
+		int count =0;
 		
-		if(str.length()%2!=0) {
-			answer = "NO";
-		} else {
-			String str1 = str.substring(0, str.length()/2);
-			String temp = str.substring(str.length()/2);
-			String[] word = temp.split("");
-			String str2 = "";
-			for(int i= temp.length()-1;i>0;i--) {
-				str2 = str2+word[i];
+		for(int i=0;i<=len;i++) {
+			if(word[i].equals(word[len])) {
+				len--;
+			} else {
+				count++;
 			}
-			if(str1.equals(str2)) { answer = "YES";}
-			else {answer = "NO";}
 		}
+		
+		if(count==0) answer="YES";
+		else answer="NO";
 		
 		return answer;
 	}
 	
 	public static void main(String[] args) {
 		Panlindrome test = new Panlindrome();
-		System.out.println(test.solution("goog"));
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		System.out.println(test.solution(str));
 
 	}
 
