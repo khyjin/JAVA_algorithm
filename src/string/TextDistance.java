bc8ba2f810aac6fec1afa1c23084245738df241b
khyjin <khyjin00@gmail.com>
2021-08-18 22:33:25 +0900

    upload

--- src/string/TextDistance.java
+++ src/string/TextDistance.java
@@ -1,17 +1,24 @@
 package string;
 
+import java.util.Scanner;
+
 public class TextDistance {
 
-	public void solution(String s, String t) {
+	public String solution(String str, String target) {
+		String answer = "";
+		//1. 왼쪽에 있는 e로부터의 거리를 구한다.(for문 이용)
 		
-		
+		//2. 오른쪽에 있는 e로부터의 거리를 구한다.(for문 이용)
 
-		
+		return answer;
 	}
 	
 	public static void main(String[] args) {
 		TextDistance test = new TextDistance();
-		test.solution("teachermode", "e");
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		String t = sc.next();
+		test.solution(s, t);
 
 	}
 
