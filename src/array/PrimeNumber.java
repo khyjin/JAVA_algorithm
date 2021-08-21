04ee1f762d39a7e801f6f45de18f1cb5035b3210
khyjin <khyjin00@gmail.com>
2021-08-21 17:41:53 +0900

    upload

---
+++ src/array/PrimeNumber.java
@@ -0,0 +1,21 @@
+package array;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+	public void solution(int num) {
+		
+		for(int i=2;i<=num;i++) {
+			
+			
+		}
+	}
+	
+	public static void main(String[] args) {
+		PrimeNumber test = new PrimeNumber();
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		test.solution(num);
+	}
+}
