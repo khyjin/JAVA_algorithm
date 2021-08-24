d8581d8cb16045325643686b624f54d92ceaa08e
KIMHYEJIN <khyjin00@gmail.com>
2021-08-24 22:53:40 +0900

    upload

--- src/array/PrimeNumber.java
+++ src/array/PrimeNumber.java
@@ -4,14 +4,18 @@ import java.util.Scanner;
 
 public class PrimeNumber {
 	public void solution(int num) {
-		int[] answer = new int[num+1];
+		int answer = 0;
+		int[] check = new int[num+1];
 		
 		for(int i=2;i<=num;i++) {
-			
-			for(int j=i;j<num;j++) {
-				
-			}
+			if(check[i]==0) {
+				answer++;
+				for(int j=i;j<=num;j=j+i) {
+					check[j] = 1;
+				}
+			}		
 		}
+		System.out.println(answer);
 	}
 	
 	public static void main(String[] args) {
