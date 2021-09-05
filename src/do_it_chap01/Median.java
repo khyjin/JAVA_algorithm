8cc5ec2509fcd3a3966d145d30b253d12f853030
KIMHYEJIN <khyjin00@gmail.com>
2021-09-05 22:01:48 +0900

    upload

--- src/do_it_chap01/Median.java
+++ src/do_it_chap01/Median.java
@@ -1,10 +1,22 @@
 package do_it_chap01;
 
+import java.util.Scanner;
+
 public class Median {
-
+	
+	//p21 실습1C-1 세값의 중앙값 구하기
+	public static int med3(int a, int b, int c) {
+		
+		return a;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		System.out.println(med3(a, b, c));
 	}
 
 }
