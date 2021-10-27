c0703ab3ab9b5a45e8407bf3c60f2c5bd586c34e
KIMHYEJIN <khyjin00@gmail.com>
2021-10-27 22:11:40 +0900

    commit

--- src/do_it_chap02/CardConvRev.java
+++ src/do_it_chap02/CardConvRev.java
@@ -1,10 +1,29 @@
 package do_it_chap02;
 
+import java.util.Scanner;
+
 public class CardConvRev {
-
+	//p67 입력받은 10진수를 2진수 ~ 36진수로 기수 변환하여 나타냄
+	//x = 10진수
+	//r = 변환할 기수
+	
+	public static String cardConvR(int x, int r) {
+		String answer = "";
+		
+		while(x>0) {
+			
+		}
+		
+		return answer;
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int r = sc.nextInt();
+		
+		System.out.println("answer = "+cardConvR(x, r));
 	}
 
 }
